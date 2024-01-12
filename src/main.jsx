f8fc5916f0c2825f184ee9a963ef9e083f20a9d7
Guillermo C Porto <guillermocporto@gmail.com>
2024-01-12 12:16:17 +0100

    Añadir archivos iniciales del proyecto, react router dom y algunas rutas iniciales con sus componentes

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/dashboard", element: <Dashboard /> },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
