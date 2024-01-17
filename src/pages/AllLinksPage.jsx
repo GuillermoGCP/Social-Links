1334b0a722d34fe77263a5298fe0a023d68d2e03
Ruth Villa <ruthvv@gmail.com>
2024-01-17 13:27:12 +0100

    prueba

--- src/pages/AllLinksPage.jsx
+++ src/pages/AllLinksPage.jsx
@@ -1,47 +1,46 @@
-import { useContext, useEffect, useState } from "react";
-
+import React from "react";
 import useApiRequest from "../hooks/useApiRequest";
+import { toast } from "react-toastify";
 import { tokenContext } from "../contexs/tokenContext";
 
 const AllLinksPage = () => {
+  const url = import.meta.env.VITE_SERVER_URL + "links";
+  const [state, setState] = React.useState();
+  const [tokenState] = React.useContext(tokenContext);
+  console.log("state", state);
+  const onSuccess = (data) => {
+    toast.success(data.data.message);
+    setState(data.data.links);
+  };
+
+  const onError = (error) => {
+    toast.error(error.message);
+  };
   const { fetchData } = useApiRequest();
-  const [allLinks, setAllLinks] = useState([]);
-  const [tokenState] = useContext(tokenContext);
-
-  useEffect(() => {
-    const fetchLinks = () => {
-      try {
-        const url = import.meta.env.VITE_SERVER_URL + "links?today=true";
-        const onSuccess = (data) => setAllLinks(data.links);
-        const onError = (error) => console.error(error);
-        const urlData = {
-          headers: {
-            Authorization: `Bearer ${tokenState}`,
-          },
-        };
-
-        fetchData(url, urlData, onSuccess, onError);
-        console.log(allLinks);
-      } catch (error) {
-        console.error("Error fetching links:", error);
-      }
+  React.useEffect(() => {
+    const urlData = {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${tokenState}`,
+      },
     };
-
-    fetchLinks();
-  }, [fetchData, tokenState]);
+    fetchData(url, urlData, onSuccess, onError);
+  }, []);
 
   return (
     <>
       <div>Links Publicados</div>
       <div>
         <ul>
-          {allLinks.map((link) => (
-            <li key={link.id}>
-              <p>{`Título: ${link.title}`}</p>
-              <p>{`URL: ${link.url}`}</p>
-              <p>{`Descripción: ${link.description}`}</p>
-            </li>
-          ))}
+          {state.map((link) => {
+            return (
+              <li key={link.id}>
+                <p>{`Título: ${link.title}`}</p>
+                <p>{`URL: ${link.url}`}</p>
+                <p>{`Description: ${link.description}`}</p>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
