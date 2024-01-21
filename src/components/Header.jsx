24a96bb48fe350693135df0da7a5fb2c78ed88ce
Luis del Saz <delsaz33@gmail.com>
2024-01-21 18:19:26 +0100

    creado logo y pintado en header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
 import ProfileBox from "./ProfileBox";
+import LogoComponent from "./LogoComponent";
 
 const Header = () => {
   return (
     <header className="bg-indigo-700 text-white p-4">
       <div className="flex items-center justify-between">
         <div>
+        <LogoComponent />
           <h1 className="text-5xl font-extrabold mb-2 mt-4 text-indigo-100">
             Links World
           </h1>
---
+++ src/components/LogoComponent.jsx
@@ -0,0 +1,11 @@
+import logoImage from "../assets/logoLinksWorld2.png";
+
+const LogoComponent = () => {
+  return (
+    <div>
+      <img src={logoImage} alt="Logo de la aplicación" className="w-40 h-40" />
+    </div>
+  );
+};
+
+export default LogoComponent;
