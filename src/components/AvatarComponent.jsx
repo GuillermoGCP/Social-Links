1f2f056e4aa326814881a640d570fc45fa520e76
Guillermo C Porto <guillermocporto@gmail.com>
2024-01-23 13:41:27 +0100

    Añadir componente avatar y estilarlo

---
+++ src/components/AvatarComponent.jsx
@@ -0,0 +1,16 @@
+import PropTypes from "prop-types";
+const AvatarComponent = ({ profileInfo }) => {
+  const urlImage =
+    import.meta.env.VITE_SERVER_URL + `/uploads/${profileInfo.profilePicture}`;
+  return (
+    <img
+      className="w-40 h-40 rounded-full"
+      src={urlImage}
+      alt={profileInfo.user}
+    />
+  );
+};
+AvatarComponent.propTypes = {
+  profileInfo: PropTypes.object,
+};
+export default AvatarComponent;
