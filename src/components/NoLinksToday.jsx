80c1fd6a3f42b5019adc430e850c609f76555fa0
Luis del Saz <delsaz33@gmail.com>
2024-01-29 18:34:43 +0100

    componente cuando no hay links

---
+++ src/components/NoLinksToday.jsx
@@ -0,0 +1,16 @@
+import sadFaceImage from "../assets/globo3.gif";
+
+const NoLinksToday = () => {
+  return (
+    <div className="flex flex-col items-center justify-center text-center h-full">
+      <p>No hay enlaces hoy</p>
+      <img
+        src={sadFaceImage}
+        alt="Logo de la aplicación"
+        className="w-1/3 h-1/3 object-contain mx-auto my-auto"
+      />
+    </div>
+  );
+};
+
+export default NoLinksToday;
