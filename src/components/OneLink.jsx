364128cf650e12c9b6775c9dd605337223649e82
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-01-30 19:10:55 +0100

    arreglar estado links de hoy para que se actualicen bien los datos al cargar la página

--- src/components/OneLink.jsx
+++ src/components/OneLink.jsx
@@ -10,7 +10,7 @@ const OneLink = ({ link, changeRating, changeRating2 }) => {
   const [, , profileInfo] = useContext(tokenContext);
   const urlImage =
     import.meta.env.VITE_SERVER_URL + `/uploads/${link.profilePicture}`;
-  console.log(link);
+
   return (
     <article className="max-w-4/5 mx-auto rounded-xl overflow-hidden shadow-lg bg-gradient-to-r from-indigo-300 p-4 ">
       <LinkDetailPost link={link} />
