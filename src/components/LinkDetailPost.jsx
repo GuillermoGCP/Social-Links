c2cca925433bf908d7610edd2fd7b0cb1999755e
Rosdany Guerra <rosdanyguerra2000@gmail.com>
2024-02-02 20:19:01 +0100

    Crear cajas para previsualizar links

--- src/components/LinkDetailPost.jsx
+++ src/components/LinkDetailPost.jsx
@@ -1,23 +1,31 @@
 const LinkDetailPost = (link) => {
   const url =
     import.meta.env.VITE_SERVER_URL + `/uploads/${link.link.profilePicture}`;
-
+  console.log(link);
   return (
-    <>
-      <article>
-        <h1>{`Título: ${link.link.title}`}</h1>
-        <h2>{`URL: ${link.link.url}`}</h2>
-        <p>{`Descripción: ${link.link.description}`}</p>
-        <p>{`Compartido por: ${link.link.name}`}</p>
-        {link.link.profilePicture ? (
+    <section className="h-[70vh]">
+      <div className="flex justify-center items-center h-screem mt-40 min-h-5">
+        <div className="flex flex-col rounded-lg bg-white shadow-[0_2px15px-3px_rgba(0,0,0,0.07),0_10px20px-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700 md:max-w-xl md:flex-row ">
           <img
-            className="w-11"
+            className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
             src={url}
-            alt="Imagen de perfil del creador del link"
+            alt=""
           />
-        ) : null}
-      </article>
-    </>
+          <div className="flex flex-col justify-start p-6">
+            <h5 className="mb-2 text-xl font-medium text-neutral-800 dark:text-neutral-50">
+              {link.link.title}
+            </h5>
+            <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
+              {link.link.description}
+            </p>
+            <p className="text-xs text-neutral-500 dark:text-neutral-300">
+              {link.link.url}
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+    
   );
 };
 export default LinkDetailPost;
--- src/pages/LinkDetailsPage.jsx
+++ src/pages/LinkDetailsPage.jsx
@@ -1,20 +1,37 @@
 import { useParams, useLocation } from "react-router-dom";
 import PropTypes from "prop-types";
 import PageNotFound from "./PageNotFound";
-import LinkDetailPost from "../components/LinkDetailPost";
+import EmbeddedPage from "./EmbeddedPage";
+import DropDown from "../components/DropDown";
 
 const LinkDetailsPage = () => {
   const location = useLocation();
   const dataLink = location?.state?.mainPageState || [];
   const { id } = useParams();
   const link = dataLink.find((p) => p.id === Number(id));
+  const url =
+    import.meta.env.VITE_SERVER_URL + "/uploads/" + link.profilePicture;
+  console.log(url);
 
   if (!link) return <PageNotFound />;
-
+  console.log(link);
   return (
-    <section>
-      <LinkDetailPost key={link.id} link={link} />
-    </section>
+    <main className=" m-20 bg-gray-100">
+      <section className="flex p-4 ">
+        {/* <article className=" bg-white p-4 rounded shadow-md h-140 items-center w-80">
+        {/* <h1>{link.title}</h1>
+          <img src={url} className="h-80"  />
+          <p>{link.url}</p>
+          <p>{link.description}</p>
+        </article> */}
+
+        <DropDown link={link} />
+
+        <div className="ml-4 bg-white p-16 rounded shadow-md flex items-center overflow-hidden">
+          <EmbeddedPage link={link} />
+        </div>
+      </section>
+    </main>
   );
 };
 
