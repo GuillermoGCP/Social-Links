0cab7eb94794699f331f3ccfeb19bccf8f9f36ec
Guillermo Cerviño Porto <144793938+GuillermoGCP@users.noreply.github.com>
2024-02-07 22:50:15 +0100

    Arreglar acortar urls de los links

--- src/hooks/useShortUrl.js
+++ src/hooks/useShortUrl.js
@@ -1,33 +1,31 @@
 import { useEffect, useState } from "react";
-import useApiRequest from "./useApiRequest";
-
+import { toast } from "react-toastify";
 const useShortUrl = (longUrl) => {
   const [shortUrl, setShortUrl] = useState("");
 
-  const onSuccess = (data) => {
-    setShortUrl(data.link);
-  };
-
   const onError = () => {
-    console.log("Error al acortar la URL");
+    toast.error("Error al acortar la URL");
   };
 
-  const url = "https://api-ssl.bitly.com/v4/shorten";
-  const apiKey = "b418ab2ba547a109b95fd8e5fffe7d0146ab9416";
-console.log(longUrl);
-
-  const { fetchData } = useApiRequest();
   useEffect(() => {
-    const urlData = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
-      },
-      body: JSON.stringify({ long_url: longUrl }),
+    const fetchShortUrl = async () => {
+      try {
+        const response = await fetch(
+          `https://tinyurl.com/api-create.php?url=${encodeURIComponent(
+            longUrl
+          )}`
+        );
+        if (!response.ok) {
+          throw new Error("Error al acortar la URL");
+        }
+        const data = await response.text();
+        setShortUrl(data);
+      } catch (error) {
+        onError();
+      }
     };
-    fetchData(url, urlData, onSuccess, onError);
-  }, [fetchData, longUrl]);
+    fetchShortUrl();
+  }, [longUrl]);
   return { shortUrl };
 };
 
